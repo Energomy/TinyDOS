0e7772d19a876943c86a325954ff6c9283d9f248
minhmc2007 <quangminh21072010@gmail.com>
2025-06-19 04:24:26 +0000

    bootloader fix, tcc will be remove soon, maybe? idk

---
+++ iso_gen_big.sh
@@ -0,0 +1,11 @@
+cp bzImage-big iso-root/boot/vmlinuz
+cp initramfs.cpio.gz iso-root/boot/initrd.gz
+genisoimage -o bootable.iso \
+    -b isolinux/isolinux.bin \
+    -c isolinux/boot.cat \
+    -no-emul-boot \
+    -boot-load-size 4 \
+    -boot-info-table \
+    -J -R -V "tinyDOS" \
+    iso-root/
+#isohybrid bootable.iso
